22dc7173722e8035120febc0491bee772cc46484
VadJS <palkinvadim@mail.ru>
2018-07-04 11:22:22 +0300

    check prettier

--- src/components/WelcomePage/index.js
+++ src/components/WelcomePage/index.js
@@ -19,10 +19,12 @@ class Welcome extends React.Component<Props> {
   };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.container }>
+        <Text style={styles.welcome      }>Welcome to React Na
+        tive!!!</Text>
+        <Text style={styles.instructions}>To get start
+        ed, edit App.js</Text>
+        <Text      style={styles.instructions}>     {instructions}        </Text>
         <Button title="BUTTON" onPress={this.toSettings} />
       </View>
     );
