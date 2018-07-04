af936b8c962951d6bfef956db1e1e73c45d3d84b
VadJS <palkinvadim@mail.ru>
2018-07-04 14:46:10 +0300

    q2

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,33 +1,33 @@
 // @flow
-import React from 'react';
+import React from "react";
 
-import { RkTextInput, RkTheme } from 'react-native-ui-kitten';
-import theme from '../../config/theme';
+import { RkTextInput, RkTheme } from "react-native-ui-kitten";
+import theme from "../../config/theme";
 
 type Props = {
-  placeholder: string,
+  placeholder: string
 };
 
-RkTheme.setType('RkTextInput', 'input', {
+RkTheme.setType("RkTextInput", "input", {
   input: {
     marginLeft: 0,
     paddingHorizontal: 10,
     fontSize: 18,
-    borderRadius: 5,
+    borderRadius: 5
   },
-  color: 'gray',
+  color: "gray",
   container: {
     marginHorizontal: 10,
     underlineWidth: 1,
-    underlineColor: theme.colors.textColor,
-  },
+    underlineColor: theme.colors.textColor
+  }
 });
 
 const BaseInput = (props: Props) => (
   <RkTextInput
     placeholder={props.placeholder}
     rkType="input"
-    props={{ placeholderTextColor: '#000' }}
+    props={{ placeholderTextColor: "#FFF" }}
     // inputStyle={{ color: '#000' }}
   />
 );
