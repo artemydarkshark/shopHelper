e318b5434c46c221563e6959dcd7cb39f0847d78
VadJS <palkinvadim@mail.ru>
2018-07-04 14:52:33 +0300

    q

--- src/App.js
+++ src/App.js
@@ -4,40 +4,28 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Welcome from './components/WelcomePage';
-import { Navigator} from './config/navigator';
+import React from "react";
+// import { Platform, StyleSheet, Text, View } from "react-native";
+// import Welcome from "./components/WelcomePage";
+import { Navigator } from "./config/navigator";
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-        <Navigator/>
-    );
-  }
-}
+export default () => <Navigator />;
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 15
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+}); */
